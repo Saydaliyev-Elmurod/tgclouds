760902a556c0b0a36761ce77f67ecf00fb00b11f
Elmurod2203 <saydaliyevelmurod01@gmail.com>
2023-04-07 06:21:31 +0500

    start project

---
+++ src/main/java/com/example/tgcloud/TgcloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.tgcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgcloudApplication.class, args);
+	}
+
+}
